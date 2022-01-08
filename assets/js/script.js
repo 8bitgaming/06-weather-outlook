3c91466672fa3a2a700bc4297612ac914d84671c
Jeremy Gieske <jeremy.gieske@gmail.com>
2022-01-08 12:48:48 -0600

    switched to w3css, improved layout and started connecting api data to DOM

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 let apiKey = 'fbf31d182481f35e3b9fc07c433c4e62'
 let pastSearch = []
 
+
 const getTodaysWeather = (city) => {
     if (city) {
         
@@ -9,6 +10,12 @@ const getTodaysWeather = (city) => {
         fetch(apiURL).then(function(response) {
             if (response.ok){
             response.json().then(function(data) {
+              var cityName = document.createElement("h3");
+              var currentDay = moment().format('L')
+              cityName.textContent = data.name;
+                   
+              // append to container
+              $("#todays-forecast").append(cityName, currentDay);
               console.log("current day", data);
             });
             }
