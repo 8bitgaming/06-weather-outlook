33182fb8295fe788d6f99e399fa00f450dad2984
Jeremy Gieske <jeremy.gieske@gmail.com>
2022-01-08 00:54:26 -0600

    initial fetch calls setup and basic html started

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+let apiKey = 'fbf31d182481f35e3b9fc07c433c4e62'
+let pastSearch = []
+
+const getTodaysWeather = (city) => {
+    if (city) {
+        
+      let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+
+        fetch(apiURL).then(function(response) {
+            if (response.ok){
+            response.json().then(function(data) {
+              console.log("current day", data);
+            });
+            }
+        }
+    
+        )}
+}
+
+const getFiveDayForecast = (city) => {
+  if (city) {
+      
+    let apiURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${apiKey}`
+
+      fetch(apiURL).then(function(response) {
+          if (response.ok){
+          response.json().then(function(data) {
+            console.log("five day", data);
+          });
+          }
+      }
+  
+      )}
+}
+
+getTodaysWeather("Minneapolis")
+getFiveDayForecast("Minneapolis")
