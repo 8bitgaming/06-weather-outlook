864eeee1e88a528e9f84ec728dc64e40dbe0b261
Jeremy Gieske <jeremy.gieske@gmail.com>
2022-01-09 01:21:45 -0600

    setup current day weather and started five day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
+//setup github secret to store/use apikey
 let apiKey = 'fbf31d182481f35e3b9fc07c433c4e62'
 let pastSearch = []
 
+//initial call to get city name and coordinates for use by the getWeather api call
 const getCityLatLong = (city) => {
   let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units&appid=${apiKey}`
 
@@ -12,10 +14,13 @@ const getCityLatLong = (city) => {
         let cityName = data.name
         getWeather(cityLat, cityLong, cityName);
       })
+    } else {
+      alert("City Not Found!")
     }
   })
 }
 
+//gets all weather data and displays on the page
 const getWeather = (cityLat, cityLong, cityName) => {
     if (cityLat && cityLong) {
         
@@ -24,12 +29,42 @@ const getWeather = (cityLat, cityLong, cityName) => {
         fetch(apiURL).then(function(response) {
             if (response.ok){
               response.json().then(function(data) {
+
                 //Update the header information for the current day
                 let currentDay = moment().format('L')
                 // let weatherIcon = data.current.weather[0].icon;
                 //http://openweathermap.org/img/wn/10d@2x.png
-                $("#current-day-header").text(`${cityName} ${currentDay}`)
-                console.log(cityName, data)
+                $("#current-day-header").text(`${cityName} (${currentDay})`)
+                
+                //create the current day weather info
+                let temp = data.current.temp;
+                let wind = data.current.wind_speed;
+                let humidity = data.current.humidity;
+                            
+                $("#current-temp").text(`Temp: ${temp}°F`);
+                $("#current-wind").text(`Wind: ${wind} MPH`);
+                $("#current-humidity").text(`Humidity: ${humidity}%`);
+
+                let uv = data.current.uvi
+                let uvBox = $("#current-uv-value")
+                uvBox.text(uv)
+
+                if (uv < 4) {
+                  uvBox.addClass("w3-green")
+                } else if (uv > 8 ) {
+                  uvBox.addClass("w3-red")
+                } else {
+                  uvBox.addClass("w3-yellow")
+                }
+
+                //create the five day forecast
+                for (let i=0; i<5; i++){
+                  // let forecastBox = document.createElement("p")
+                  // forecastBox.id = `weather-day-${[i]}`
+                  // forecastBox.addClass("w3-card w3-blue")
+                  $("#day"+i).text("Day")
+                  // $("#day"+[i]).append()
+                }
                 
               });
             }
@@ -37,21 +72,8 @@ const getWeather = (cityLat, cityLong, cityName) => {
     }    
   }
 
+  //create event listener for search
+
+  //create history of past searches and store to local storage
+
 getCityLatLong("Minneapolis")
-
-
-
-
-              // let cityName = data.name;
-              // 
-              // 
-              // let temp = data.main.temp;
-              // let wind = data.wind.speed;
-              // let humidity = data.main.humidity;
-              
-              
-              // cityName.textContent = ;
-              
-          
-              // $("#todays-forecast").append(cityName, currentDay);
-              // console.log("current day", data);
